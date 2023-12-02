ee2e3dc4361044e44d47b3cd003815a75fbf98bc
w1ndstr5ck <84208375+w1ndstr5ck@users.noreply.github.com>
2023-12-01 19:56:47 -0600

    Final Day 1 Updates

--- Day_1/1_1.py
+++ Day_1/1_1.py
@@ -1,3 +1,22 @@
 
-file1 = open("C:\Users\roger\Documents\GitHub\Advent_Of_Code_2023\Day_1\input.txt", "r")
+file1 = open("Input1_1.txt", "r")
+lines = file1.readlines()
 
+total = 0
+for line in lines:
+    for char in line:
+        if char.isdigit():
+            start = char
+            break
+        else:
+            continue
+    for char in reversed(line):
+        if char.isdigit():
+            last = char
+            break
+        else:
+            continue
+    combined = start + last
+    total += int(combined)
+
+print(total)
